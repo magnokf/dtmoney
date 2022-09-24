4fa2ca0b660bb8dec495f41339468ac49fdbf30c
magnokf <mkfelipe.rj@gmail.com>
2022-09-24 09:31:59 -0300

    chore: styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
 
-function App() {
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+  `;
+export function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <Title>Hello World, this is my first styled component!</Title>
+      <GlobalStyle />
 
-export default App;
+    </div>
+  )
+}
