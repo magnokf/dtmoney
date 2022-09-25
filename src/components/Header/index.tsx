001d4664c162e4d50c48c379e7e573382e0e5e60
magnokf <mkfelipe.rj@gmail.com>
2022-09-25 19:55:53 -0300

    feat: create NewTransactionModal Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,19 @@
-import { useState } from 'react';
-import Modal from 'react-modal';
-import logoImg from '../../assets/logo.svg'; 
-import { Container, Content } from './styles';
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
 
-export function Header() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+interface HeaderProps {
+	onOpenNewTransactionModal: () => void;
+}
 
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true);
-  }
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false);
-  }
-  return (
-    <Container>
-      <Content>
-        <img src={logoImg} alt="dt money" />
-        <button type="button" onClick={handleOpenNewTransactionModal}>
-          Nova Transação
-        </button>
-        <Modal 
-          isOpen={isNewTransactionModalOpen} 
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar Transação</h2>
-        </Modal>
-      </Content>
-    </Container>
-  );
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+	return (
+		<Container>
+			<Content>
+				<img src={logoImg} alt="dt money" />
+				<button type="button" onClick={onOpenNewTransactionModal}>
+					Nova Transação
+				</button>
+			</Content>
+		</Container>
+	);
 }
