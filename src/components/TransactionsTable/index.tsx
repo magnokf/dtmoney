8a090cba45f49a648b2394ba77946364ce8249ba
magnokf <mkfelipe.rj@gmail.com>
2022-09-26 17:51:43 -0300

    seeds setup

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -28,24 +28,7 @@ export function TransactionsTable(){
               <td>Desenvolvimento</td>
               <td>20/02/2021</td>
             </tr>
-            <tr>
-              <td>Desenvolvimento de website</td>
-              <td className="deposit">R$12.000</td>
-              <td>Desenvolvimento</td>
-              <td>20/02/2021</td>
-            </tr>
-            <tr>
-              <td>Aluguel Loja A</td>
-              <td className="withdraw">R$12.000</td>
-              <td>Aluguel</td>
-              <td>20/02/2021</td>
-            </tr>
-            <tr>
-              <td>Desenvolvimento de website</td>
-              <td className="deposit">R$12.000</td>
-              <td>Desenvolvimento</td>
-              <td>20/02/2021</td>
-            </tr>
+            
         </tbody>
           
       </table>
